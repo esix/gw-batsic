9c8a0d0e95bd34cd88b26ec1f56abf9b1dfee6f4
esix <esix@live.ru>
2023-03-16 10:57:18 +0300

    handling special symbols in chr::FromAscii

--- lib/buffer.bat
+++ lib/buffer.bat
@@ -1,10 +1,13 @@
 @echo off
-shift & goto :%~1
+if not "%~1"=="" shift & goto :%~1
+goto :_start
+
 
 :ToString buffer ret
   setlocal EnableDelayedExpansion
   set "buffer=%~1"
   set "i=0"
+  set ret=
 
   if "!buffer!"=="" goto ToString__LoopEnd
 
@@ -14,9 +17,11 @@ shift & goto :%~1
     if "!c!"=="" goto ToString__LoopEnd
     set /a  i = i + 1
 
-    call int HexToInt n !c!
+    call byte ByteToDec !c! n
+    call chr FromAscii !n! ascii
 
-    call chr FromAscii ascii !n!
+    ::echo "Next char=!c! -> !n! -> !ascii!"
+    ::echo "ret=!ret!!ascii!"
     set "ret=!ret!!ascii!"
 
     goto ToString__Loop
@@ -26,3 +31,23 @@ shift & goto :%~1
 
   endlocal && set "%~2=%ret%"
 exit /b 0
+
+
+:_start
+  setlocal EnableDelayedExpansion
+  set PATH=%~dp0..\tests;%~dp0;%PATH%
+  set "numTests=0"
+  set "passedTests=0"
+  set "failedTests=0"
+  call:_test
+  echo Total tests: %numTests%
+  echo      FAILED: %failedTests%
+  echo      PASSED: %passedTests%
+  endlocal
+exit /b
+
+:_test
+  set __=
+  :: 1090 print "                 HELLO WORLD!!!"
+  call expect "buffer ToString 31303930207072696E742022202020202020202020202020202020202048454C4C4F20574F524C4421212122 __" "1090 print ""                 HELLO WORLD""
+exit /b
--- tests/expect.bat
+++ tests/expect.bat
@@ -7,13 +7,28 @@ set /a numTests+=1
 call %operation%
 set "e=%ERRORLEVEL%"
 
-if "%~2" neq "%__%" (
+set isFailed=
+set expected=%~2
+
+setlocal EnableDelayedExpansion
+if "!expected!" neq "!__!" (
   echo FAILED: "%operation%"
   echo   Expected result = "%~2"
+  ::set __
   echo                __ = "%__%"   err=%e%
   echo.
+  endlocal && set isFailed=T
+) else (
+  endlocal
+)
+
+
+if "%isFailed%"=="T" (
   set /a failedTests+=1
-) else set /a passedTests+=1
+) else (
+  set /a passedTests+=1
+)
+set isFailed=
 
 exit /B
 
