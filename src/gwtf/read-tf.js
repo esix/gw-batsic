d43099d9a7d76114ebf824f488762e8019451802
esix <esix@live.ru>
2023-10-04 19:58:25 +0300

    tokens

--- src/gwtf/read-tf.js
+++ src/gwtf/read-tf.js
@@ -3,8 +3,9 @@ const binary = fs.readFileSync('./B.BAS');
 
 let i = 0;
 let v;
+let report, hex = '';
 
-const byte = () => (v = binary[i++]);
+const byte = () => { v = binary[i++]; hex += x(v) + ' '; return v;};
 const word = () => byte() + 0x100 * byte();
 const dword = () => word() + 0x10000 * word();
 const qword = () => dword() + 0x100000000 * dword();
@@ -16,16 +17,17 @@ const nextByte = () => (v = binary[i]);
 const x = (v) => (v & 0xFF).toString(16).padStart(2, '0');
 const xx = (v) => x(v) + x(v >> 8);
 
+// read first byte
 if (byte() !== 0xff) {
   console.log('First byte is', v);
   return;
 }
 
-let report;
 
 const assert = (exp, expl) => {
   if (!exp) {
     console.error('Assertion failed:', expl);
+    console.log(hex + '... ' + x(byte()) + ' ' + x(byte()) + ' ' + x(byte()));
     console.log(report);
     process.exit(0);
   }
@@ -35,6 +37,7 @@ const assert = (exp, expl) => {
 while (true) {
   let offset = word();
   if (offset === 0x0000) break;
+  hex = '';
   const lineNum = word();
   report = lineNum + ' ';
 
@@ -67,7 +70,25 @@ while (true) {
 
         case 0x20: report += ' '; break;
 
+        case 0x22: report += '"'; state = "STR"; break;
+        case 0x24: report += '$'; break;
+
+        case 0x28: report += '('; break;
+        case 0x29: report += ')'; break;
+
         case 0x2C: report += ','; break;
+
+        case 0x30: // 0
+        case 0x31:
+        case 0x32:
+        case 0x33:
+        case 0x34:
+        case 0x35:
+        case 0x36:
+        case 0x37:
+        case 0x38:
+        case 0x39: report += String.fromCharCode(v); break;    // 9
+
 
         case 0x3a:                        // '   (stored as 3A8FD9, ":REM'" but the ":REM" is suppressed when the program is listed.)
           if (nextByte() === 0x8f) {
@@ -80,6 +101,8 @@ while (true) {
             report += ':';
           }
           break;
+        case 0x3B: report += ';'; break;
+
 
         case 0x41:    // A
         case 0x42:
@@ -106,8 +129,7 @@ while (true) {
         case 0x57:
         case 0x58:
         case 0x59:
-        case 0x5A:    // Z
-          report += String.fromCharCode(v); break;
+        case 0x5A: report += String.fromCharCode(v); break;       // Z
 
         case 0x81: report += 'END'; break;
         case 0x82: report += 'FOR'; break;
@@ -157,7 +179,10 @@ while (true) {
         case 0xAE: report += 'DEFSNG'; break;
         case 0xAF: report += 'DEFDBL'; break;
         case 0xB0: report += 'LINE'; break;
-        case 0xB1: assert(0, 'B1'); break;            // WHILE   (stored as B1E9, "WHILE+" but the "+" is suppressed when the program is listed.)
+        case 0xB1:             // WHILE   (stored as B1E9, "WHILE+" but the "+" is suppressed when the program is listed.)
+          assert(byte() === 0xE9, `WHILE stored as B1 ${x(v)}`);
+          report += 'WHILE';
+          break;
         case 0xB2: report += 'WEND'; break;
         case 0xB3: report += 'CALL'; break;
         case 0xB4: assert(0, '(B4 - Undefined)'); break;
@@ -233,19 +258,116 @@ while (true) {
         case 0xFA: assert(0, '(FA - Undefined)'); break;
         case 0xFB: assert(0, '(FB - Undefined)'); break;
         case 0xFC: assert(0, '(FC - Undefined)'); break;
-
+        case 0xFD:
+          switch (byte()) {
+            case 0x81: report += 'CVI'; break;
+            case 0x82: report += 'CVS'; break;
+            case 0x83: report += 'CVD'; break;
+            case 0x84: report += 'MKI$'; break;
+            case 0x85: report += 'MKS$'; break;
+            case 0x86: report += 'MKD$'; break;
+            case 0x8B: report += 'EXTERR'; break;
+            default: assert(0, `Unknown: FD ${x(v)}`); break;
+          }
+          break;
+        case 0xFE:
+          switch (byte()) {
+            case 0x81: report += 'FILES'; break;
+            case 0x82: report += 'FIELD'; break;
+            case 0x83: report += 'SYSTEM'; break;
+            case 0x84: report += 'NAME'; break;
+            case 0x85: report += 'LSET'; break;
+            case 0x86: report += 'RSET'; break;
+            case 0x87: report += 'KILL'; break;
+            case 0x88: report += 'PUT'; break;
+            case 0x89: report += 'GET'; break;
+            case 0x8A: report += 'RESET'; break;
+            case 0x8B: report += 'COMMON'; break;
+            case 0x8C: report += 'CHAIN'; break;
+            case 0x8D: report += 'DATE$'; break;
+            case 0x8E: report += 'TIME$'; break;
+            case 0x8F: report += 'PAINT'; break;
+            case 0x90: report += 'COM'; break;
+            case 0x91: report += 'CIRCLE'; break;
+            case 0x92: report += 'DRAW'; break;
+            case 0x93: report += 'PLAY'; break;
+            case 0x94: report += 'TIMER'; break;
+            case 0x95: report += 'ERDEV'; break;
+            case 0x96: report += 'IOCTL'; break;
+            case 0x97: report += 'CHDIR'; break;
+            case 0x98: report += 'MKDIR'; break;
+            case 0x99: report += 'RMDIR'; break;
+            case 0x9A: report += 'SHELL'; break;
+            case 0x9B: report += 'ENVIRON'; break;
+            case 0x9C: report += 'VIEW'; break;
+            case 0x9D: report += 'WINDOW'; break;
+            case 0x9E: report += 'PMAP'; break;
+            case 0x9F: report += 'PALETTE'; break;
+            case 0xA0: report += 'LCOPY'; break;
+            case 0xA1: report += 'CALLS'; break;
+            case 0xA4: report += 'NOISE'; break;    // NOISE   (PCjr only) /     DEBUG   (Sperry PC only)
+            case 0xA5: report += 'PCOPY'; break;    // (PCjr or EGA system only)
+            case 0xA6: report += 'TERM'; break;     // (PCjr only)
+            case 0xA7: report += 'LOCK'; break;
+            case 0xA8: report += 'UNLOCK'; break;
+            default: assert(0, `Unknown: FE ${x(v)}`); break;
+          }
+          break;
+        case 0xFF:
+          switch (byte()) {
+            case 0x81: report += 'LEFT$'; break;
+            case 0x82: report += 'RIGHT$'; break;
+            case 0x83: report += 'MID$'; break;
+            case 0x84: report += 'SGN'; break;
+            case 0x85: report += 'INT'; break;
+            case 0x86: report += 'ABS'; break;
+            case 0x87: report += 'SQR'; break;
+            case 0x88: report += 'RND'; break;
+            case 0x89: report += 'SIN'; break;
+            case 0x8A: report += 'LOG'; break;
+            case 0x8B: report += 'EXP'; break;
+            case 0x8C: report += 'COS'; break;
+            case 0x8D: report += 'TAN'; break;
+            case 0x8E: report += 'ATN'; break;
+            case 0x8F: report += 'FRE'; break;
+            case 0x90: report += 'INP'; break;
+            case 0x91: report += 'POS'; break;
+            case 0x92: report += 'LEN'; break;
+            case 0x93: report += 'STR$'; break;
+            case 0x94: report += 'VAL'; break;
+            case 0x95: report += 'ASC'; break;
+            case 0x96: report += 'CHR$'; break;
+            case 0x97: report += 'PEEK'; break;
+            case 0x98: report += 'SPACE$'; break;
+            case 0x99: report += 'OCT$'; break;
+            case 0x9A: report += 'HEX$'; break;
+            case 0x9B: report += 'LPOS'; break;
+            case 0x9C: report += 'CINT'; break;
+            case 0x9D: report += 'CSNG'; break;
+            case 0x9E: report += 'CDBL'; break;
+            case 0x9F: report += 'FIX'; break;
+            case 0xA0: report += 'PEN'; break;
+            case 0xA1: report += 'STICK'; break;
+            case 0xA2: report += 'STRIG'; break;
+            case 0xA3: report += 'EOF'; break;
+            case 0xA4: report += 'LOC'; break;
+            case 0xA5: report += 'LOF'; break;
+            default: assert(0, `Unknown: FF ${x(v)}`); break;
+          }
+          break;
         default:
-          assert(0, 'Unknown byte ' + x(v));
+          assert(0, 'Unknown: ' + x(v));
       }
     } else if (state === 'REM') {
-      if (byte() === 0x00) {
-        state = 'EOL';
-      } else {
-        report += String.fromCharCode(v);
-      }
+      if (byte() === 0x00) state = 'EOL';
+      else report += String.fromCharCode(v);
+    } else if (state === "STR") {
+      if (byte() === 0x22) { report += '"'; state = "START"; }
+      else report += String.fromCharCode(v);
     }
   }
 
+  // console.log(hex);
   console.log(report);
 }
 
