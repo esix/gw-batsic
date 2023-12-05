21aaede267fa161844f9fad273f7c8d332ffa74d
esix <esix@live.ru>
2023-12-05 10:50:11 +0300

    check

--- src/num/_xhalf.test.bat
+++ src/num/_xhalf.test.bat
@@ -1,7 +1,20 @@
 
-echo xhalf test
-
 call %test% "xhalf.check"
-  call expect "_xhalf check 0" "1"
-  call %expect% "_xhalf check 1" "1"
-  call %expect% "_xhalf check 2" "1"
+  call expecterr "_xhalf check 0" 0
+  call expecterr "_xhalf check 1" 0
+  call expecterr "_xhalf check 2" 0
+  call expecterr "_xhalf check 1" 0
+  call expecterr "_xhalf check 2" 0
+  call expecterr "_xhalf check 9" 0
+  call expecterr "_xhalf check A" 0
+  call expecterr "_xhalf check B" 0
+  call expecterr "_xhalf check F" 0
+  call expecterr "_xhalf check a" 1
+  call expecterr "_xhalf check b" 1
+  call expecterr "_xhalf check f" 1
+  call expecterr "_xhalf check g" 1
+  call expecterr "_xhalf check G" 1
+  call expecterr "_xhalf check :" 1
+   call expecterr "_xhalf check" 1
+  call expecterr "_xhalf check 00" 1
+  call expecterr "_xhalf check ^" ^""  1
